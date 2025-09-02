1ca9ca6ff3d35d4096f0b5a9f866662f5987a481
anajulia0911 <anajblande04@gmail.com>
2025-09-02 23:42:08 +0000

    commit 1

---
+++ lab03/src/CriterioPrecoMaximo.java
@@ -0,0 +1,15 @@
+public class CriterioPrecoMaximo implements CriterioBusca{
+
+    @Override
+    public boolean testar (Produto p, String valor){
+        try {
+            double precoMaximo = Double.parseDouble(valor);
+            return p.getPreco() <= precoMaximo;
+        } catch (NumberFormatException e) {
+            return false; 
+        }
+    }
+}
+    
+    
+
