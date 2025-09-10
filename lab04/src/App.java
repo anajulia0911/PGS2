993cc4998b6879a650a02c4782da118001d30e52
anajulia0911 <anajblande04@gmail.com>
2025-09-10 00:30:12 +0000

    COMMIT 2

--- lab04/src/App.java
+++ lab04/src/App.java
@@ -3,12 +3,11 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) {
         try {
-            
             Class.forName("org.postgresql.Driver");
 
             System.out.println("Proprietarios!");
@@ -20,14 +19,14 @@ public class App {
             System.out.println("Conexão ok!");
 
             // INSERT
-            inserirProprietario(con, "Ana", "235656565-65");
+            inserirProprietario(con, "Julia", "235656565-65");
             inserirProprietario(con, "Carlos", "123456789-00");
 
             // SELECT
             listarProprietarios(con);
 
             // UPDATE
-            atualizarProprietario(con, 1, "Ana Maria", "987654321-00");
+            atualizarProprietario(con, 1, "Ana ", "987654321-00");
 
             listarProprietarios(con);
 
@@ -46,54 +45,58 @@ public class App {
     // CREATE
     public static void inserirProprietario(Connection con, String nome, String cpf) throws SQLException {
         String sql = "INSERT INTO PROPRIETARIOS (nome, cpf) VALUES (?, ?)";
-        PreparedStatement pstmt = con.prepareStatement(sql);
-        pstmt.setString(1, nome);
-        pstmt.setString(2, cpf);
-        int qte = pstmt.executeUpdate();
-        if (qte > 0) {
-            System.out.println("Proprietário inserido com sucesso!");
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setString(1, nome);
+            pstmt.setString(2, cpf);
+            int qte = pstmt.executeUpdate();
+            if (qte > 0) {
+                System.out.println("Proprietário inserido com sucesso!");
+            }
         }
     }
 
-    // READ
+    //LISTAR
     public static void listarProprietarios(Connection con) throws SQLException {
         System.out.println("\n--- Lista de Proprietários ---");
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM PROPRIETARIOS ORDER BY id");
-        while (rs.next()) {
-            int id = rs.getInt("id");
-            String nome = rs.getString("nome");
-            String cpf = rs.getString("cpf");
-            System.out.println("ID: " + id + " | Nome: " + nome + " | CPF: " + cpf);
+        String sql = "SELECT * FROM PROPRIETARIOS ORDER BY id";
+        try (Statement stmt = con.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String nome = rs.getString("nome");
+                String cpf = rs.getString("cpf");
+                System.out.println("ID: " + id + " | Nome: " + nome + " | CPF: " + cpf);
+            }
         }
-        System.out.println("-------------------------------\n");
+        
     }
 
     // UPDATE
     public static void atualizarProprietario(Connection con, int id, String novoNome, String novoCpf) throws SQLException {
         String sql = "UPDATE PROPRIETARIOS SET nome = ?, cpf = ? WHERE id = ?";
-        PreparedStatement pstmt = con.prepareStatement(sql);
-        pstmt.setString(1, novoNome);
-        pstmt.setString(2, novoCpf);
-        pstmt.setInt(3, id);
-        int qte = pstmt.executeUpdate();
-        if (qte > 0) {
-            System.out.println("Proprietário atualizado com sucesso!");
-        } else {
-            System.out.println("Proprietário com ID " + id + " não encontrado!");
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setString(1, novoNome);
+            pstmt.setString(2, novoCpf);
+            pstmt.setInt(3, id);
+            int qte = pstmt.executeUpdate();
+            if (qte > 0) {
+                System.out.println("Proprietário atualizado com sucesso!");
+            } else {
+                System.out.println("Proprietário com ID " + id + " não encontrado!");
+            }
         }
     }
 
     // DELETE
     public static void removerProprietario(Connection con, int id) throws SQLException {
         String sql = "DELETE FROM PROPRIETARIOS WHERE id = ?";
-        PreparedStatement pstmt = con.prepareStatement(sql);
-        pstmt.setInt(1, id);
-        int qte = pstmt.executeUpdate();
-        if (qte > 0) {
-            System.out.println("Proprietário removido com sucesso!");
-        } else {
-            System.out.println("Proprietário com ID " + id + " não encontrado!");
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            int qte = pstmt.executeUpdate();
+            if (qte > 0) {
+                System.out.println("Proprietário removido com sucesso!");
+            } else {
+                System.out.println("Proprietário com ID " + id + " não encontrado!");
+            }
         }
     }
 }
